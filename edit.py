a3ef19c06ede2dc554c46aee064be287950708a2
TrueStrikes <137821671+TrueStrikes@users.noreply.github.com>
2023-07-18 12:03:52 -0400

    Update edit.py
    e

--- edit.py
+++ edit.py
@@ -2,6 +2,8 @@ import os
 import json
 import subprocess
 from colorama import init, Fore, Style
+import clipboard
+import time
 
 # Initialize colorama
 init()
@@ -42,13 +44,49 @@ def modify_config():
 
     print_message('Items modified successfully!', Fore.GREEN)
 
-    try:
-        subprocess.Popen(['cmd', '/c', 'start', 'python', os.path.join(script_dir, 'main.py')], shell=True)
-        print_message('main.py opened successfully!', Fore.YELLOW)
-    except Exception as e:
-        print_message(f'An error occurred while opening main.py: {str(e)}', Fore.RED)
-
     return True
+
+def detect_clipboard_changes():
+    print_message("Searching started!", Fore.YELLOW)
+
+    processed_items = []  # List to store processed item IDs
+    cooldown_duration = 5  # Cooldown period in seconds
+    last_detection_time = 0  # Time of the last detection
+    previous_clipboard = ""  # Previous clipboard content
+
+    while True:
+        current_clipboard = clipboard.paste()
+
+        # Check if clipboard content has changed
+        if current_clipboard != previous_clipboard:
+            # Automatically enter detected item ID
+            url = current_clipboard.strip()
+            if url.startswith("https://www.roblox.com/catalog/"):
+                item_id = url.replace("https://www.roblox.com/catalog/", "").split("/")[0]
+
+                # Check if cooldown period has passed since the last detection
+                current_time = time.time()
+                if current_time - last_detection_time >= cooldown_duration:
+                    if item_id not in processed_items:  # Check if item ID is new
+                        print("Detected Item ID:", item_id)
+                        processed_items.append(item_id)  # Add item ID to the processed list
+                        # Automatically enter the item ID into the config file or perform desired actions
+
+                        try:
+                            subprocess.Popen(['cmd', '/c', 'start', 'python', os.path.join(script_dir, 'main.py')], shell=True)
+                            print_message('main.py opened successfully!', Fore.YELLOW)
+                        except Exception as e:
+                            print_message(f'An error occurred while opening main.py: {str(e)}', Fore.RED)
+
+                    # Update the last detection time and previous clipboard
+                    last_detection_time = current_time
+                    previous_clipboard = current_clipboard
+
+        # Add a delay to avoid constant polling
+        time.sleep(1)
+
+# Call the function to detect clipboard changes
+detect_clipboard_changes()
 
 # Infinite loop
 while modify_config():
