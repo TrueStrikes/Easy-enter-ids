ad2171857e9fe6170643fdee24b097c9801a076d
TrueStrikes <137821671+TrueStrikes@users.noreply.github.com>
2023-07-18 08:18:04 -0400

    Update edit.py
    e43

--- edit.py
+++ edit.py
@@ -15,45 +15,44 @@ file_path = os.path.join(script_dir, 'config.json')
 # Define the input prompt color
 input_prompt_color = Fore.LIGHTBLUE_EX
 
+# Prompt message for item input
+prompt_message = f"{input_prompt_color}Enter the URL/itemID (or 'exit' to quit): {Style.RESET_ALL}"
+
+# Check if config.json exists
+if not os.path.isfile(file_path):
+    print('config.json file not found in the script directory.')
+    exit()
+
+# Read the JSON file
+with open(file_path, 'r') as file:
+    data = json.load(file)
+
+# Check if "items" section exists
+if "items" not in data:
+    print('"items" section not found in the JSON file.')
+    exit()
+
 # Infinite loop
 while True:
-    # Check if config.json exists
-    if os.path.isfile(file_path):
-        # Read the JSON file
-        with open(file_path, 'r') as file:
-            data = json.load(file)
+    print(prompt_message, end="")
+    url = input()
 
-        # Search for the "items" section
-        if "items" in data:
-            # Prompt for item input
-            print(f"{input_prompt_color}Enter the URL/itemID (or 'exit' to quit): {Style.RESET_ALL}", end="")
-            url = input()
+    if url.lower() == "exit":
+        break
 
-            if url.lower() == "exit":
-                break  # Exit the loop
+    url = url.replace("https://www.roblox.com/catalog/", "").split("/")[0]
+    data["items"] = [url]
 
-            # Remove unnecessary parts from the URL
-            url = url.replace("https://www.roblox.com/catalog/", "")
-            url = url.split("/")[0]  # Remove anything after the first slash
+    # Save the modified JSON data back to the file
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
 
-            # Modify the contents of the "items" list with the cleaned URL
-            data["items"] = [url]
+    # Glow effect
+    print(f"{Fore.GREEN}{Style.BRIGHT}Items modified successfully!{Style.RESET_ALL}")
 
-            # Save the modified JSON data back to the file
-            with open(file_path, 'w') as file:
-                json.dump(data, file, indent=4)
-
-            # Glow effect
-            print(f"{Fore.GREEN}{Style.BRIGHT}Items modified successfully!{Style.RESET_ALL}")
-
-            try:
-                # Execute main.py in a separate command prompt window
-                main_file_path = os.path.join(script_dir, 'main.py')
-                subprocess.Popen(['cmd', '/c', 'start', 'python', main_file_path], shell=True)
-                print(f"{Fore.YELLOW}main.py opened successfully!{Style.RESET_ALL}")
-            except Exception as e:
-                print('An error occurred while opening main.py:', str(e))
-        else:
-            print('"items" section not found in the JSON file.')
-    else:
-        print('config.json file not found in the script directory.')
+    try:
+        main_file_path = os.path.join(script_dir, 'main.py')
+        subprocess.Popen(['cmd', '/c', 'start', 'python', main_file_path], shell=True)
+        print(f"{Fore.YELLOW}main.py opened successfully!{Style.RESET_ALL}")
+    except Exception as e:
+        print('An error occurred while opening main.py:', str(e))
