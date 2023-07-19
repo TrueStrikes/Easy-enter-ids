ed07818514303eed7da071ea75a696f42648ea05
TrueStrikes <137821671+TrueStrikes@users.noreply.github.com>
2023-07-19 16:25:48 -0400

    Update log.py

--- log.py
+++ log.py
@@ -1,89 +1,115 @@
 import os
 import json
 import subprocess
+import pyperclip
+from colorama import init, Fore, Style
+import time
 from tkinter import *
-from tkinter import messagebox
+
+# Initialize colorama
+init()
 
 # Get the script's directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, 'config.json')
 log_file_path = os.path.join(script_dir, 'log.json')
+
+# Define the input prompt color
+input_prompt_color = Fore.LIGHTBLUE_EX
+
+def print_message(message, color):
+    print(f"{color}{message}{Style.RESET_ALL}")
+
+def modify_config(url):
+    if not os.path.isfile(file_path):
+        print_message('config.json file not found in the script directory.', Fore.RED)
+        return False
+
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    if "items" not in data:
+        print_message('"items" section not found in the JSON file.', Fore.RED)
+        return False
+
+    url = url.replace("https://www.roblox.com/catalog/", "").replace("https://web.roblox.com/catalog/", "").split("/")[0]
+    data["items"] = [url]
+
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+
+    print_message('Items modified successfully!', Fore.GREEN)
+
+    return True
+
+def update_log(log_file_path, item_id, item_name):
+    log_data = load_logs()
+
+    if {"id": item_id, "name": item_name} not in log_data:
+        log_data.append({"id": item_id, "name": item_name})
+        save_logs(log_data)
 
 def load_logs():
     if os.path.isfile(log_file_path):
         with open(log_file_path, 'r') as log_file:
             try:
                 log_data = json.load(log_file)
+                return log_data
             except json.JSONDecodeError:
-                log_data = []
-    else:
-        log_data = []
+                print_message("Invalid log file format. Creating a new log file.", Fore.YELLOW)
+    return []
 
-    return log_data
+def save_logs(log_data):
+    with open(log_file_path, 'w') as log_file:
+        json.dump(log_data, log_file, indent=4)
 
 def create_gui():
-    def open_item():
-        selected_item = listbox.get(ANCHOR)
-        item = json.loads(selected_item)
-        item_id = item["id"]
-        subprocess.Popen(['cmd', '/c', 'start', 'python', 'main.py', item_id], shell=True)
+    logs = load_logs()
 
-    def delete_item():
-        selected_item = listbox.get(ANCHOR)
-        item = json.loads(selected_item)
-        item_id = item["id"]
-
-        log_data = load_logs()
-        log_data = [item for item in log_data if item["id"] != item_id]
-
-        with open(log_file_path, 'w') as log_file:
-            json.dump(log_data, log_file, indent=4)
-
-        update_listbox()
-        messagebox.showinfo("Success", "Item deleted successfully.")
-
-    def refresh():
-        log_data = load_logs()
-        update_listbox()
-
-    def update_listbox():
-        listbox.delete(0, END)
-        log_data = load_logs()
-        for item in log_data:
-            item_name = item["name"]
-            listbox.insert(END, item_name)
-
-    # Create the main window
     root = Tk()
     root.title("Logged Items")
-    root.configure(bg="black")
+    root.configure(background="black")
 
-    # Create the listbox
-    listbox = Listbox(root, font=("Arial", 12), bg="black", fg="yellow")
-    listbox.pack(pady=10)
-
-    # Create a scrollbar
     scrollbar = Scrollbar(root)
     scrollbar.pack(side=RIGHT, fill=Y)
 
-    # Set the scrollbar to the listbox
+    listbox = Listbox(root, width=50, height=20, font=("Courier New", 12), bg="black", fg="yellow", selectbackground="black", selectforeground="yellow")
+    listbox.pack(side=LEFT, fill=BOTH)
     listbox.config(yscrollcommand=scrollbar.set)
     scrollbar.config(command=listbox.yview)
 
-    # Add buttons
-    open_button = Button(root, text="Open Item", command=open_item)
-    open_button.pack(pady=5)
+    for item in logs:
+        listbox.insert(END, item["name"])
 
-    delete_button = Button(root, text="Delete Item", command=delete_item)
-    delete_button.pack(pady=5)
+    def open_item():
+        selected_item = listbox.get(listbox.curselection())
+        item = next((item for item in logs if item["name"] == selected_item), None)
+        if item:
+            item_id = item["id"]
+            subprocess.Popen(['cmd', '/c', 'start', 'python', os.path.join(script_dir, 'main.py'), item_id], shell=True)
 
-    refresh_button = Button(root, text="Refresh", command=refresh)
-    refresh_button.pack(pady=5)
+    def delete_item():
+        selected_item = listbox.get(listbox.curselection())
+        item = next((item for item in logs if item["name"] == selected_item), None)
+        if item:
+            logs.remove(item)
+            save_logs(logs)
+            refresh_list()
 
-    # Load the initial items in the listbox
-    update_listbox()
+    def refresh_list():
+        listbox.delete(0, END)
+        for item in logs:
+            listbox.insert(END, item["name"])
 
-    # Start the GUI main loop
+    open_button = Button(root, text="Open", width=10, command=open_item)
+    open_button.pack(side=LEFT, padx=5, pady=10)
+
+    delete_button = Button(root, text="Delete", width=10, command=delete_item)
+    delete_button.pack(side=LEFT, padx=5, pady=10)
+
+    refresh_button = Button(root, text="Refresh", width=10, command=refresh_list)
+    refresh_button.pack(side=LEFT, padx=5, pady=10)
+
     root.mainloop()
 
-# Call the create_gui function to start the application
 create_gui()
